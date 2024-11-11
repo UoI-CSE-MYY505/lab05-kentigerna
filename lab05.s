b30c6d5444aed69181bdbb39dacbe28007736499
kentigerna <pennyrenia@gmail.com>
2024-11-12 00:34:02 +0200

    done

--- lab05.s
+++ lab05.s
@@ -1,3 +1,5 @@
+#Aikaterini Panagiota Katsanou
+
 # ----------------------------------------------------------------------------------------
 # lab05.s 
 #  Verifies the correctness of some aspects of a 5-stage pipelined RISC-V implementation
